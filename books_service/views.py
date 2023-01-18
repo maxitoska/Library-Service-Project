f29f635f693316ab3ef19bdf0690ccd02102ea88
Maks Danylchuk <maxitosm00@gmail.com>
2023-01-18 05:54:19 +0200

    Completed task "Only admin users can create/update/delete books" with created IsAdminOrIfAuthenticatedReadOnly method in books_service permissions.py

--- books_service/views.py
+++ books_service/views.py
@@ -2,11 +2,12 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
 from books_service.models import Book
+from books_service.permissions import IsAdminOrIfAuthenticatedReadOnly
 from books_service.serializers import BookSerializer
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
